89419cf331ab29b685d691340e1946538d5824fb
Myrmekes-a <nightfurry090@gmail.com>
2023-04-21 14:38:38 +0900

    feat: add api for update authority sign

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -1,9 +1,13 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import { getStakedNFTsFromWallet } from './scripts';
+import express from "express";
+import { Request, Response } from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cors from "cors";
 import { NowRequest, NowResponse } from "@vercel/node";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { rebirthTxSign } from "./scripts";
 
 const app = express();
 
@@ -14,20 +18,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
-app.get('/staked-nfts/*', async (req, res) => {
-  const address = req.params[0];
-  if (!address) res.status(404).send('Invalid address');
-  else {
-    console.log(address);
-    const result = await getStakedNFTsFromWallet(address as string);
-    console.log(result);
-    if (!result) res.status(502).send('Internal server error');
-    else res.send(result);
+
+app.get("/", async (req: Request, res: Response) => {
+  res.status(404).send("Sign SAGC Update Authority BE is running");
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  const { tx } = req.body;
+  if (!tx) {
+    return res.status(400).json({ err: "Invalid tx request" });
+  }
+
+  try {
+    const result = await rebirthTxSign(req.body.tx);
+
+    return res.status(200).json(result);
+  } catch (e: any) {
+    console.error(e.message);
+
+    return res.status(500).json(e);
   }
 });
+
 let port = process.env.PORT ?? 80;
 server.listen(port, async () => {
-  console.log('--@ Start: Listening on http://localhost:', port);
+  console.log("--@ Start: Listening on http://localhost:", port);
 });
 
 //module.exports = app
